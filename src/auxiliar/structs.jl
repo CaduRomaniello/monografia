0b79867eb0b33f069c87b4f07f40ec969e5be3cc
CaduRomaniello <cadu_romaniello@hotmail.com>
2022-12-17 16:27:48 -0300

    fixed JSON

--- src/auxiliar/structs.jl
+++ src/auxiliar/structs.jl
@@ -18,7 +18,7 @@ mutable struct Classroom
     isLab::Bool
     capacity::Int64
     buildingID::Int64
-    desciption::String
+    description::String
     floor::Int64
     board::String
     projector::Bool
@@ -72,4 +72,36 @@ mutable struct Reservation
     classroomID::Int64
     dayOfWeek::Int64
     scheduleID::Int64
+end
+
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+
+mutable struct Cell
+    working::Bool
+    meetings::Array{Int64, 1}
+end
+
+mutable struct ProfessorWorkTime
+    ID::String
+    monday::Array{Cell, 1}
+    tuesday::Array{Cell, 1}
+    wednesday::Array{Cell, 1}
+    thursday::Array{Cell, 1}
+    friday::Array{Cell, 1}
+    saturday::Array{Cell, 1}
+end
+
+mutable struct Objective
+    isPractical::Bool
+    dayOfWeek::Int64
+    subjectID::String
+    classesIDs::Array{String, 1}
+    schedules::Array{Int, 1}
+    vacancies::Int64
+    demand::Int64
+    professors::Array{String, 1}
 end
